59af8b85afbaf89294be3da83fcae5bf52e85f9d
Leet <76251617+skyline523@users.noreply.github.com>
2024-06-14 17:14:54 +0800

    feat: 添加判断设备和监听屏幕尺寸变化

--- package/utils/device.ts
+++ package/utils/device.ts
@@ -5,7 +5,7 @@ import { getUserAgent } from './ua'
  *
  * @category device
  */
-export const isBrowser = typeof window !== 'undefined'
+export const isBrowser = typeof window !== 'undefined' && typeof window.document !== 'undefined'
 
 /**
  * Checks if the code is being executed in a server environment
@@ -30,4 +30,81 @@ export function isMobile() {
   if (!isBrowser)
     return false
   return mobileDevicesRegExp.test(getUserAgent())
+}
+
+/**
+ * Regular expression pattern to match tablet device user agents
+ *
+ * @category device
+ */
+export const tabletDevicesRegExp = /iPad|android(?!.*mobile)|tablet|kindle|silk|playbook|(windows(?!.*phone)(.*touch))|(puffin(?!.*(IP|AP|WP)))/i
+
+/**
+ * Check if the code is running on a tablet device
+ * Tablet also is kind of mobile device
+ *
+ * @category device
+ */
+export function isTablet() {
+  if (!isBrowser)
+    return false
+  if (!isMobile())
+    return false
+  return tabletDevicesRegExp.test(getUserAgent())
+}
+
+/**
+ * Check if the code is running on a desktop device
+ *
+ * @category device
+ */
+export function isDesktop() {
+  if (!isBrowser)
+    return false
+  return !isMobile()
+}
+
+/**
+ * Regular expression pattern to match apple device user agents
+ *
+ * @category device
+ */
+export const appleDevicesRegExp = /iPhone|iPad|iPod|Mac/i
+
+/**
+ * Check if the code is running on a apple device
+ *
+ * @category device
+ */
+export function isApple() {
+  if (!isBrowser)
+    return false
+  return appleDevicesRegExp.test(getUserAgent())
+}
+
+interface DeviceResizeWatcherOptions {
+  immediate: boolean
+}
+
+/**
+ * Check if the code is running on a android device
+ *
+ * @category device
+ */
+export const isAndroid = /Android/i.test(getUserAgent())
+
+export function watchResize(
+  callback: () => void,
+  { immediate }: DeviceResizeWatcherOptions = { immediate: true },
+) {
+  if (!isBrowser)
+    return
+  if (immediate)
+    window.addEventListener('load', callback, false)
+
+  window.addEventListener(
+    'orientationchange' in window ? 'orientationchange' : 'resize',
+    callback,
+    false,
+  )
 }
